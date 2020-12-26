5bbfa5704824b797b8431c7cc5e47c55338e1e61
ebpark9511 <ebpark9511@gmail.com>
2020-12-26 22:33:24 +0900

    ✨ Add asset get code

---
+++ Soyeon/Resources/Colors.swift
@@ -0,0 +1,28 @@
+//
+//  UIColors.swift
+//  Soyeon
+//
+//  Created by 박은비 on 2020/12/26.
+//  Copyright © 2020 ludus. All rights reserved.
+//
+
+import Foundation
+import UIKit.UIColor
+
+enum Colors: String {
+    case bgNormal
+    case buttonDisabled
+    case darkGray
+    case soyeonBlue
+    case soyeonPurple
+    case strongBlack
+}
+
+extension Colors {
+    func color() -> UIColor {
+        guard let color = UIColor(named: self.rawValue)
+        else { return UIColor.white }
+        
+        return color
+    }
+}
