4ed2ce659931d74a868c9a12b5501769c30047f8
ebpark9511 <ebpark9511@gmail.com>
2021-01-31 22:00:02 +0900

    🩹 Load Fonts

--- Soyeon/Supporting Files/AppDelegate.swift
+++ Soyeon/Supporting Files/AppDelegate.swift
@@ -15,6 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        Fonts.install(from: Bundle.allBundles)
         return true
     }
 
