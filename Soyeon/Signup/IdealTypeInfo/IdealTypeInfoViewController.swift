3e6b201fb87516e3a9b79b81b24830a994a35ff3
junyng <pom0894@naver.com>
2021-03-13 15:57:53 +0900

    Add IdealTypeInfoViewController (#74)

---
+++ Soyeon/Signup/IdealTypeInfo/IdealTypeInfoViewController.swift
@@ -0,0 +1,31 @@
+//
+//  IdealTypeInfoViewController.swift
+//  Soyeon
+//
+//  Created by junyeong-cho on 2021/03/13.
+//  Copyright © 2021 ludus. All rights reserved.
+//
+
+import UIKit
+
+class IdealTypeInfoViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setNavigationTitle("이상형 정보 수정하기")
+    }
+        
+    private func setupLayout() {
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
