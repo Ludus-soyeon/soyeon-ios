d1b4777a850e110cb4bba90b5860d0ea97c70511
evie <ebpark9511@gmail.com>
2021-04-27 13:36:32 +0900

    ✨ Add Intro Animation

---
+++ Soyeon/Extension/UIApplication+.swift
@@ -0,0 +1,15 @@
+//
+//  UIApplication.swift
+//  Soyeon
+//
+//  Created by 박은비 on 2021/04/27.
+//  Copyright © 2021 ludus. All rights reserved.
+//
+
+import UIKit
+
+extension UIApplication {
+    var soyeonWindow: UIWindow? {
+        return UIApplication.shared.windows.filter {$0.isKeyWindow}.first
+    }
+}
