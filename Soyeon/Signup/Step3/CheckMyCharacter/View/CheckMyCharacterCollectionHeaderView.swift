8f416854f74d6ccb37f97063e544bed62495058d
ebpark9511 <ebpark9511@gmail.com>
2021-05-26 11:43:39 +0900

    ✨ Add Check Character UI

---
+++ Soyeon/Signup/Step3/CheckMyCharacter/View/CheckMyCharacterCollectionHeaderView.swift
@@ -0,0 +1,13 @@
+//
+//  CheckMyCharacterCollectionHeaderView.swift
+//  Soyeon
+//
+//  Created by 박은비 on 2021/05/26.
+//  Copyright © 2021 ludus. All rights reserved.
+//
+
+import UIKit
+
+class CheckMyCharacterCollectionHeaderView: UICollectionReusableView {
+    
+}
